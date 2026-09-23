9bd7c9070e2c076a93c52ca8f2a1ef7019d4f1cb
Artem Ishchenko <artem.ischchenko@gmail.com>
2026-09-23 18:08:18 +0300

    Розділ 18: фінальна версія Smart Life 1.0

--- SmartLife/app/build.gradle.kts
+++ SmartLife/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.17"
+        versionName = "1.0"
 
         // "Запускач" інструментальних (UI) тестів на пристрої/емуляторі
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
